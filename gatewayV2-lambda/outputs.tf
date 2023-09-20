ef1c7c1d05315e48c31fe2af5f61640a0089cbcf
Mateus Attie <attie.mateus@gmail.com>
2023-09-20 15:03:53 -0300

    modified:   outputs.tf

--- gatewayV2-lambda/outputs.tf
+++ gatewayV2-lambda/outputs.tf
@@ -15,7 +15,7 @@ output "apigatewayv2_api_arn" {
 
 output "apigatewayv2_api_execution_arn" {
   description = "The ARN prefix to be used in an aws_lambda_permission's source_arn attribute or in an aws_iam_policy to authorize access to the @connections API."
-  value       = taws_apigatewayv2_api.attielabz-gateway.execution_arn
+  value       = aws_apigatewayv2_api.attielabz-gateway.execution_arn
 }
 
 # default stage
@@ -26,12 +26,12 @@ output "default_apigatewayv2_stage_id" {
 
 output "default_apigatewayv2_stage_arn" {
   description = "The default stage ARN"
-  value       = aws_apigatewayv2_stage.dattielabz-gateway-stage.arn
+  value       = aws_apigatewayv2_stage.attielabz-gateway-stage.arn
 }
 
 output "default_apigatewayv2_stage_execution_arn" {
   description = "The ARN prefix to be used in an aws_lambda_permission's source_arn attribute or in an aws_iam_policy to authorize access to the @connections API."
-  value       = aws_apigatewayv2_stage.dattielabz-gateway-stage.execution_arn
+  value       = aws_apigatewayv2_stage.attielabz-gateway-stage.execution_arn
 }
 
 output "default_apigatewayv2_stage_invoke_url" {
